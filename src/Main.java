a3933dfd310b9a93e95685da87f08ae78fa71b8b
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-10-21 15:03:03 -0400

    Started Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+
 /**
  * File created by jcdesimp on 10/17/14.
  */
 public class Main {
     public static void main(String [ ] args) {
-       //todo implement main
+
+        int NUM_TEAMS = 3;
+        int NUM_MEMBERS = 4;
+
+
+        Manager manager = new Manager();
+
+        ArrayList<ArrayList<Employee>> teams = new ArrayList<ArrayList<Employee>>();
+
+
+        for (int t = 0; t < NUM_TEAMS; t++) {
+
+            ArrayList<Employee> currTeam = new ArrayList<Employee>();
+
+            for (int m = 0; m < NUM_MEMBERS; m++) {
+                //currTeam.add();
+            }
+
+        }
     }
 }
