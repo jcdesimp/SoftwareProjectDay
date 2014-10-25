c5822e895c75559b7562fbf2bdfd463f3bd402bf
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-10-24 23:39:13 -0400

    set up office constructor w/o timeTracker

--- src/Office.java
+++ src/Office.java
@@ -11,5 +11,20 @@ public class Office {
     private ArrayList<Team> teams;
     private ConferenceRoom confRoom;
 
+    public Office() {
+        this.logger =  new OfficeLogger();
 
+        //this.timeTracker = new TimeTracker()
+
+        this.projectManager = new Manager(this);
+        this.confRoom =  new ConferenceRoom();
+        
+        this.teams = new ArrayList<Team>();
+        for (int i = 1; i < 4; i++) {
+            teams.add( new Team(i, this) );
+        }
+
+
+
+    }
 }
