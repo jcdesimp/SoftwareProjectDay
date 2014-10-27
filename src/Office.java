10a3e5291e4e239a286ada094a7d1198391a03cc
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-10-26 21:05:26 -0400

    uncommented initial team meeting calls

--- src/Office.java
+++ src/Office.java
@@ -42,6 +42,7 @@ public class Office {
         }
     }
 
+    /*
     public void holdLeadMeeting(int teamId, Office office, Developer employee)
     {
 
@@ -76,6 +77,7 @@ public class Office {
             office.getLogger().logAtTime(end_message);
         }
     }
+    */
     public void startDay() {
         timeTracker.start();
         projectManager.start();
