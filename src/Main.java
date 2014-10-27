92cb5823c703038dc401a6a79fdf25b049f6ca6a
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-10-26 23:03:43 -0400

    minor change to main commented code

--- src/Main.java
+++ src/Main.java
@@ -21,27 +21,5 @@ public class Main {
         startSignal.countDown();
 
 
-
-        /*
-        int NUM_TEAMS = 3;
-        int NUM_MEMBERS = 4;
-
-
-        ArrayList<ArrayList<Employee>> teams = new ArrayList<ArrayList<Employee>>();
-
-
-        for (int t = 0; t < NUM_TEAMS; t++) {
-
-            ArrayList<Employee> currTeam = new ArrayList<Employee>();
-
-            for (int m = 0; m < NUM_MEMBERS; m++) {
-                //currTeam.add();
-            }
-
-        }
-        */
-
-
-
     }
 }
