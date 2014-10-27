bf3381b76f14bb1f4183a1c44f17cecf4fc5f2a4
Tyler <tsg7741@gmail.com>
2014-10-26 20:04:12 -0400

    Started the lead meeting
    Still needs to be flushed out

--- src/Office.java
+++ src/Office.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
-
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
 /**
  * File created by jcdesimp on 10/24/14.
  */
@@ -11,8 +12,9 @@ public class Office {
     private Manager projectManager;
     private ArrayList<Team> teams;
     private ConferenceRoom confRoom;
+    ArrayList<Integer> teamLeads;
 
-
+    private CyclicBarrier leadMeeting = new CyclicBarrier(4);
 
     public Office(TimeTracker time, CountDownLatch startSignal) {
 
@@ -29,6 +31,53 @@ public class Office {
 
     }
 
+    public void setLeadMeeting()
+    {
+        try {
+            leadMeeting.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void holdLeadMeeting(int teamId, Office office, Developer employee)
+    {
+
+        while(!teamLeads.contains(teamId))
+        {
+            if () //if all team leads are here
+            {
+                synchronized (this)
+                {
+
+                    System.out.println("Waiting" + employee.getTeam().getTeamId() + employee.getDevId());
+                    String gather_message = "Team " + teamId + " is gathering for the lead meeting.";
+                    office.getLogger().logAtTime(gather_message);
+                }
+            }
+        }
+
+        if (employee.getDevId() == 1)
+        {
+            String start_message = "Team " + teamId + " is starting the meeting.";
+            office.getLogger().logAtTime(start_message);
+
+        }
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (employee.getDevId() == 1)
+        {
+            String end_message = "Team " + teamId + " has ended the lead meeting.";
+            office.getLogger().logAtTime(end_message);
+        }
+    }
+    
     public void startDay() {
         timeTracker.start();
         projectManager.start();
